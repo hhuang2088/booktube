88de2da49565b6e1ee7783d8e2664ddd5419a950
hhuang2088 <hhuang2088@gmail.com>
2019-01-23 18:05:13 -0800

    imported google books client to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import googleBooks from '../apis/googleBooks';
+
+const KEY = 'AIzaSyCgOn17Vmj2Pxml9gS2XFgCx7p8fEru_bM';
 
 class App extends React.Component {
 	render() {
