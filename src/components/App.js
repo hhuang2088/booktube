c05faf9c706d91ee8a75b5e4de384f4e7649a964
hhuang2088 <hhuang2088@gmail.com>
2019-01-23 18:32:27 -0800

    imported Searchbar component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import googleBooks from '../apis/googleBooks';
+import Searchbar from './Searchbar';
 
 const KEY = 'AIzaSyCgOn17Vmj2Pxml9gS2XFgCx7p8fEru_bM';
 
 class App extends React.Component {
 	render() {
 		return (
-			<h1>Hello App</h1>
+			<Searchbar />
 		)
 	}
 }
