7fc9f20767bb24ac14e59d8c63c516f37df96a86
hhuang2088 <hhuang2088@gmail.com>
2019-01-23 18:46:56 -0800

    created searchbar form that connects to Searchbar state

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class Searchbar extends React.Component {
+	state = {term: ''};
+
+	onSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state.term);
+	}
+
+	onChange = (event) => {
+		this.setState({term: event.target.value});
+	}
+
 	render() {
 		return (
-			<h1>Hello Searchbar</h1>
+			<div className="ui segment">
+				<form onSubmit={this.onSubmit} className="ui form">
+					<div className="field">
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={this.onChange}
+						/>
+					</div>
+				</form>
+			</div>
 		)
 	}
 }
