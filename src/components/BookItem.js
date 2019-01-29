f0fc20bfb04325d91bbb870444c3a9a88348b664
hhuang2088 <hhuang2088@gmail.com>
2019-01-28 22:52:57 -0800

    fixed bug where site would crash if api call didn't return some details of particular books

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import './BookItem.css';
 
+const getBookInfo = (book) => {
+	const bookInfo = {
+		author: [],
+		image: 'http://www.formica.com/us/~/media/global-images/ui/noimageavailable.png',
+		publisher: '',
+		title: '',
+		infoLink: ''
+	};
+		if (book.volumeInfo)
+		{
+			if (book.volumeInfo.authors) {
+				 bookInfo.author = book.volumeInfo.authors;
+			}
+			if (book.volumeInfo.imageLinks) {
+				if (book.volumeInfo.imageLinks.thumbnail) {
+					bookInfo.image = book.volumeInfo.imageLinks.thumbnail;
+				}
+			}
+			if (book.volumeInfo.publisher) {
+				bookInfo.publisher = book.volumeInfo.publisher;
+			}
+			if (book.volumeInfo.title) {
+				bookInfo.title = book.volumeInfo.title;
+			}
+			if (book.volumeInfo.infoLink) {
+				bookInfo.infoLink = book.volumeInfo.infoLink;
+			}
+	}
+	return (bookInfo);
+}
+
 const BookItem = ({book}) => {
+	console.log(book);
+	const bookInfo = getBookInfo(book);
 	return(
 		<div className="book-item item">
 			<img
 				className="ui image"
-				src={book.volumeInfo.imageLinks.thumbnail}
-				alt={book.volumeInfo.title}
+				src={bookInfo.image}
+				alt={bookInfo.title}
 			/ >
 			<div className="content">
-				<a className="header" href={book.volumeInfo.infoLink}>{book.volumeInfo.title}</a>
+				<a className="header" href={bookInfo.infoLink}>{bookInfo.title}</a>
 				<div className="description">
-					Author(s): {book.volumeInfo.authors.join(', ')}
+					Author(s): {bookInfo.author.join(', ')}
 					<br />
-					Publisher: {book.volumeInfo.publisher}
+					Publisher: {bookInfo.publisher}
 				</div>
 			</div>
 		</div>
