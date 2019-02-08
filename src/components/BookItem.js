7d5b4919ae390ac29a68d4d181113236458f3d8a
hhuang2088 <hhuang2088@gmail.com>
2019-02-07 21:05:13 -0800

    put getBookInfo function into it's own file and imported it into BookItem file

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,36 +1,6 @@
 import React from 'react';
-import 'components/BookItem.css';
-
-const getBookInfo = (book) => {
-	const bookInfo = {
-		author: [],
-		image: 'http://www.formica.com/us/~/media/global-images/ui/noimageavailable.png',
-		publisher: '',
-		title: '',
-		infoLink: ''
-	};
-		if (book.volumeInfo)
-		{
-			if (book.volumeInfo.authors) {
-				 bookInfo.author = book.volumeInfo.authors;
-			}
-			if (book.volumeInfo.imageLinks) {
-				if (book.volumeInfo.imageLinks.thumbnail) {
-					bookInfo.image = book.volumeInfo.imageLinks.thumbnail;
-				}
-			}
-			if (book.volumeInfo.publisher) {
-				bookInfo.publisher = book.volumeInfo.publisher;
-			}
-			if (book.volumeInfo.title) {
-				bookInfo.title = book.volumeInfo.title;
-			}
-			if (book.volumeInfo.infoLink) {
-				bookInfo.infoLink = book.volumeInfo.infoLink;
-			}
-	}
-	return (bookInfo);
-}
+import getBookInfo from 'utils/getBookInfo';
+import './BookItem.css';
 
 const BookItem = ({book}) => {
 	const bookInfo = getBookInfo(book);
---
+++ src/utils/getBookInfo.js
@@ -0,0 +1,32 @@
+const getBookInfo = (book) => {
+	const bookInfo = {
+		author: [],
+		image: 'http://www.formica.com/us/~/media/global-images/ui/noimageavailable.png',
+		publisher: '',
+		title: '',
+		infoLink: ''
+	};
+		if (book.volumeInfo)
+		{
+			if (book.volumeInfo.authors) {
+				 bookInfo.author = book.volumeInfo.authors;
+			}
+			if (book.volumeInfo.imageLinks) {
+				if (book.volumeInfo.imageLinks.thumbnail) {
+					bookInfo.image = book.volumeInfo.imageLinks.thumbnail;
+				}
+			}
+			if (book.volumeInfo.publisher) {
+				bookInfo.publisher = book.volumeInfo.publisher;
+			}
+			if (book.volumeInfo.title) {
+				bookInfo.title = book.volumeInfo.title;
+			}
+			if (book.volumeInfo.infoLink) {
+				bookInfo.infoLink = book.volumeInfo.infoLink;
+			}
+	}
+	return (bookInfo);
+}
+
+export default getBookInfo;
