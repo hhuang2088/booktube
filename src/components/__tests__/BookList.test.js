1583f7075be1080aa4dcb0b652d83221b4a758fe
hhuang2088 <hhuang2088@gmail.com>
2019-02-07 23:32:19 -0800

    added tests for BookList.test.js

---
+++ src/components/__tests__/BookList.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BookList from 'components/BookList';
+
+let	wrapper = shallow(<BookList />);
+
+describe('BookList component without books props', () => {
+	describe('the div', () => {
+		const div = wrapper.find('div');
+
+		it('there should only be one div', () => {
+			expect(div.length).toEqual(1);
+		});
+
+		it('displays the right notice', () => {
+			expect(div.text()).toEqual('Books not found, please enter another search term');
+		})
+	});
+});
