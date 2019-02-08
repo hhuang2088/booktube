da29dab7e48cd1a097ebb215ee5cb9d4e28d4c51
hhuang2088 <hhuang2088@gmail.com>
2019-02-07 22:31:33 -0800

    wrote tests for the App component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import Searchbar from '../Searchbar';
-import BookList from '../BookList';
+import App from 'components/App';
+import Searchbar from 'components/Searchbar';
+import BookList from 'components/BookList';
 
-let wrapped;
+let wrapper;
 
 beforeEach(() => {
-	wrapped = shallow(<App />)
+	wrapper = shallow(<App />)
+});
+
+afterEach(() => {
+	wrapper.unmount();
+})
+
+it('returns a div with className ui container', () => {
+	expect(wrapper.find('div').length).toEqual(1);
+	expect(wrapper.find('div').hasClass('ui')).toBeTruthy();
+	expect(wrapper.find('div').hasClass('container')).toBeTruthy();
 })
 
 it('shows a Searchbar', () => {
-	expect(wrapped.find(Searchbar).length).toEqual(1);
+	expect(wrapper.find(Searchbar).length).toEqual(1);
 })
 
 it('shows a BookList', () => {
-	expect(wrapped.find(BookList).length).toEqual(1);
+	expect(wrapper.find(BookList).length).toEqual(1);
+})
+
+it('has a books state that is set to an empty array', () => {
+	expect(wrapper.state().books).toBeTruthy();
+	expect(wrapper.state().books.length).toEqual(0);
+})
+
+it('has an onFormSubmit method', () => {
+	expect(wrapper.instance().onFormSubmit);
 })
