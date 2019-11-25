0098ede5695347b2b910cf3725c33dfdd6a0d1b2
hhuang2088 <hhuang2088@gmail.com>
2019-11-24 23:33:30 -0800

    Added ui grid classes to BookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -11,7 +11,7 @@ const BookList = ({ books, addBookToReadingList, readingList }) => {
   const firstFiveBooks = books.slice(0, 5);
   const displayBooks = firstFiveBooks.map(book => {
     return (
-      <div className="ui grid container">
+      <div className="ui grid">
         <BookListItem
           book={book}
           key={book.id}
